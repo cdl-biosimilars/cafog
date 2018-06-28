1bf812798e590c84aa4dd6d75fad82914413b95c
wolfgang <wolfgang.skala@sbg.ac.at>
2018-06-28 13:44:11 +0200

    change pos of read_clean_datasets()

--- correct_glycation.py
+++ correct_glycation.py
@@ -15,6 +15,38 @@ from glycan import PTMComposition
 from glycoprotein import Glycoprotein
 
 
+def read_clean_datasets(filename: str) -> pd.Series:
+    """
+    Read input datasets (glycoforms, glycations) and prepare for analysis,
+    i.e., generate a single column containing abundances with uncertainties.
+
+    :param str filename: name of the file containing the dataset
+    :return: a series called "abundance" containing a value with uncertainty
+             and an index named "index_col"
+    :rtype: pd.Series
+    :raises ValueError: if the input dataset contains too few columns
+    """
+
+    df = pd.read_csv(filename, comment="#", index_col=0, header=None)
+    col_count = df.shape[1]
+
+    if col_count == 0:  # too few columns
+        raise ValueError("{} contains too few columns.".format(filename))
+    elif col_count == 1:  # add error column
+        logging.warning(
+            "{} lacks a column containing errors. Assuming errors of zero."
+            .format(filename))
+        df["auto_error_column"] = 0
+    elif col_count > 2:  # remove surplus columns
+        logging.warning(
+            "{} contains {} additional columns, which will be ignored."
+            .format(filename, col_count-2))
+        df = df.iloc[:, :3]
+    df.index.name = "index_col"
+    df["abundance"] = df.apply(lambda r: ufloat(r.iloc[0], r.iloc[1]), axis=1)
+    return df["abundance"]
+
+
 def calc_glycation_graph(glycan_library: str,
                          glycoforms: pd.Series,
                          glycation: pd.Series) -> nx.DiGraph:
@@ -194,38 +226,6 @@ def save_graph(G: nx.DiGraph,
         nx.write_gexf(G, "{}_corr.gexf".format(filename))
 
 
-def read_clean_datasets(filename: str) -> pd.Series:
-    """
-    Read input datasets (glycoforms, glycations) and prepare for analysis,
-    i.e., generate a single column containing abundances with uncertainties.
-
-    :param str filename: name of the file containing the dataset
-    :return: a series called "abundance" containing a value with uncertainty
-             and an index named "index_col"
-    :rtype: pd.Series
-    :raises ValueError: if the input dataset contains too few columns
-    """
-
-    df = pd.read_csv(filename, comment="#", index_col=0, header=None)
-    col_count = df.shape[1]
-
-    if col_count == 0:  # too few columns
-        raise ValueError("{} contains too few columns.".format(filename))
-    elif col_count == 1:  # add error column
-        logging.warning(
-            "{} lacks a column containing errors. Assuming errors of zero."
-            .format(filename))
-        df["auto_error_column"] = 0
-    elif col_count > 2:  # remove surplus columns
-        logging.warning(
-            "{} contains {} additional columns, which will be ignored."
-            .format(filename, col_count-2))
-        df = df.iloc[:, :3]
-    df.index.name = "index_col"
-    df["abundance"] = df.apply(lambda r: ufloat(r.iloc[0], r.iloc[1]), axis=1)
-    return df["abundance"]
-
-
 if __name__ == "__main__":
     logging.basicConfig(format="%(levelname)s: %(message)s",
                         level=logging.INFO)
@@ -239,8 +239,7 @@ if __name__ == "__main__":
                         version="%(prog)s 1.0")
     parser.add_argument("-o", "--output-format",
                         action="store",
-                        help="graph output format, either 'dot' or 'gexf' "
-                             "(default: 'dot')",
+                        help="graph output format, either 'dot' or 'gexf' ",
                         metavar="FORMAT",
                         choices=["dot", "gexf"])
     parser.add_argument("-l", "--glycan-library",
