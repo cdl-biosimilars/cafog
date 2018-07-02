331ea2b2d24290fb9ebd5c657a8331ede892ee57
wolfgang <wolfgang.skala@sbg.ac.at>
2018-07-02 11:00:51 +0200

    implement load glycation

--- correct_glycation.py
+++ correct_glycation.py
@@ -11,40 +11,9 @@ import networkx as nx
 import pandas as pd
 from uncertainties import ufloat
 
+from correction import read_clean_datasets
 from glycan import PTMComposition
 from glycoprotein import Glycoprotein
-
-
-def read_clean_datasets(filename: str) -> pd.Series:
-    """
-    Read input datasets (glycoforms, glycations) and prepare for analysis,
-    i.e., generate a single column containing abundances with uncertainties.
-
-    :param str filename: name of the file containing the dataset
-    :return: a series called "abundance" containing a value with uncertainty
-             and an index named "index_col"
-    :rtype: pd.Series
-    :raises ValueError: if the input dataset contains too few columns
-    """
-
-    df = pd.read_csv(filename, comment="#", index_col=0, header=None)
-    col_count = df.shape[1]
-
-    if col_count == 0:  # too few columns
-        raise ValueError("{} contains too few columns.".format(filename))
-    elif col_count == 1:  # add error column
-        logging.warning(
-            "{} lacks a column containing errors. Assuming errors of zero."
-            .format(filename))
-        df["auto_error_column"] = 0
-    elif col_count > 2:  # remove surplus columns
-        logging.warning(
-            "{} contains {} additional columns, which will be ignored."
-            .format(filename, col_count-2))
-        df = df.iloc[:, :3]
-    df.index.name = "index_col"
-    df["abundance"] = df.apply(lambda r: ufloat(r.iloc[0], r.iloc[1]), axis=1)
-    return df["abundance"]
 
 
 def calc_glycation_graph(glycan_library: str,
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 
-
 import pandas as pd
 from PyQt5.QtChart import (QBarCategoryAxis, QBarSeries, QBarSet,
                            QChart, QValueAxis)
 from PyQt5.QtCore import QLocale, QMargins
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QWidget
 
+from correction import read_clean_datasets
 
 from main_window import Ui_MainWindow
 from widgets import FileTypes, get_filename
@@ -34,39 +34,58 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         # instance attributes
-        self.df_glycation = None
+        self.se_glycation = None
         self.last_path = None
 
         # actions
         self.btLoadGlycation.clicked.connect(self.load_glycation)
         self.btQuit.clicked.connect(QApplication.instance().quit)
 
+        # GUI modifications
+        self.lbGlycation.setText("")
+        self.lbGlycoform.setText("")
         self.cvGlycation.chart().setBackgroundRoundness(0)
         self.cvGlycation.chart().layout().setContentsMargins(0, 0, 0, 0)
+        self.cvGlycoforms.chart().setBackgroundRoundness(0)
+        self.cvGlycoforms.chart().layout().setContentsMargins(0, 0, 0, 0)
+        self.cvResults.chart().setBackgroundRoundness(0)
+        self.cvResults.chart().layout().setContentsMargins(0, 0, 0, 0)
 
-    def load_glycation(self):
-        # filename, _, self.last_path = get_filename(
-        #     self, "open", "Load glycation data …",
-        #     self.last_path, FileTypes(["csv"]))
-        # if filename is None:
-        #     return
-        #
-        # try:
-        #     self.df_glycation = pd.read_csv(filename)
-        # except OSError as e:
-        #     QMessageBox.critical(
-        #         self,
-        #         "Error",
-        #         "Error when reading {}: {}".format(filename, e))
-        #     return
+    def load_glycation(self) -> None:
+        """
+        Load glycation data from a CSV file and display it
+        in the corresponding chart view.
 
+        :return: nothing, sets self.se_glycation
+        :rtype: None
+        """
+
+        # load and clean glycation data
+        filename, _, self.last_path = get_filename(
+            self, "open", "Load glycation data …",
+            self.last_path, FileTypes(["csv"]))
+        if filename is None:
+            return
+
+        try:
+            self.se_glycation = read_clean_datasets(filename)
+        except (OSError, ValueError) as e:
+            QMessageBox.critical(self, "Error", str(e))
+            return
+
+        # extract x- and y-values from series
+        x_values = [str(i) for i in self.se_glycation.index]
+        y_values = [a.nominal_value for a in self.se_glycation]
+
+        # assemble the chart
         bar_set = QBarSet("bar_set")
-        bar_set.append([75, 15, 5, 3, 2])
+        bar_set.append(y_values)
+        bar_set.hovered.connect(self.update_glycation_label)
         bar_series = QBarSeries()
         bar_series.append(bar_set)
 
         x_axis = QBarCategoryAxis()
-        x_axis.append(["0", "1", "2", "3", "4"])
+        x_axis.append(x_values)
         x_axis.setTitleText("count")
 
         y_axis = QValueAxis()
@@ -84,6 +103,27 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         chart.layout().setContentsMargins(0, 0, 0, 0)
         chart.setMargins(QMargins(5, 5, 5, 5))
         self.cvGlycation.setChart(chart)
+
+    def update_glycation_label(self,
+                               hover: bool,
+                               bar_index: int) -> None:
+        """
+        Display information on glycation abundance
+        of the bar under the cursor.
+
+        :param bool hover: True if the mouse hovers over a bar; false otherwise
+        :param int bar_index: index of the bar that triggered the signal
+        :return: nothing
+        :rtype: None
+        """
+
+        if hover:
+            self.lbGlycation.setText(
+                "Abundance: <b>{:.2f}</b> ± {:.2f} %".format(
+                    self.se_glycation[bar_index].nominal_value,
+                    self.se_glycation[bar_index].std_dev))
+        else:
+            self.lbGlycation.setText("")
 
 
 def _main():
